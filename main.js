c88b2210acad12b518dfc410461d18bc64f25c7e
Tyler May <tylermay@Tylers-MacBook-Pro.local>
2017-08-01 18:13:08 -0500

    finished customer data

--- main.js
+++ main.js
@@ -9,30 +9,36 @@ for (let i = 0; i < customers.results.length; i++) {
   let img = document.createElement("img")
   img.src = customer.picture.medium
 
-  let first = document.createElement("label")
-  first.innerHTML = `${customer.name.first} ${customer.name.last}`
+  let firstLast = document.createElement("label")
+  firstLast.innerHTML = `${customer.name.first} ${customer.name.last}`
+  firstLast.className = "name"
 
   let email = document.createElement("label")
   email.innerHTML = customer.email
+  email.className = "email"
 
-  let customerInfo = document.createElement("label")
-  customerInfo.innerHTML = `${customer.location.street} ${customer.location.city} ${customer.location.state} ${customer.location.postcode} ${customer.phone}`
+  let address = document.createElement("label")
+  address.innerHTML = `${customer.location.street}`
+  address.className = "address"
+
+  let cityState = document.createElement("label")
+  cityState.innerHTML = `${customer.location.city}, ${customer.location.state} ${customer.location.postcode}`
+  cityState.className = "cityState"
+
+  let phoneNumber = document.createElement("label")
+  phoneNumber.innerHTML = `${customer.phone}`
+  phoneNumber.className = "phone"
 
   let ssn = document.createElement("label")
   ssn.innerHTML = customer.id.value
+  ssn.className = "social"
 
   div.appendChild(img)
-  div.appendChild(first)
+  div.appendChild(firstLast)
   div.appendChild(email)
-  div.appendChild(customerInfo)
+  div.appendChild(address)
+  div.appendChild(cityState)
+  div.appendChild(phoneNumber)
   div.appendChild(ssn)
   document.body.appendChild(div)
 }
-
-
-const page = `
-<head>INTERNAL COMPANY DIRECTORY</head>
-`;
-
-let header = document.createElement("header")
-header.innerHTML = page
